889d345fb2b8e75770f2a3a578a09154b1a32f2e
Yassin <yassinmohamedyehia1817@gmail.com>
2026-07-06 13:24:19 +0300

    Added filter

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send("App connected successfully");
-  console.log ("test");
+  console.log ("filter");
 });
 
 export default app;
